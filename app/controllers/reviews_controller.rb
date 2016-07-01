f3b200e5b5b285b6e606e7c02133e8057d061226
Jason Jordan <jtjordan13@gmail.com>
2016-07-01 13:41:43 -0400

    Can write reviews for books

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,48 @@
+class ReviewsController < ApplicationController
+
+  def show
+
+  end
+
+  def index
+
+  end
+
+
+  def new
+    @book = Book.find params[:book_id]
+    @review = @book.reviews.new(user_id: current_user.id)
+  end
+
+  def create
+    @book = Book.find params[:book_id]
+    @review = @book.reviews.new approved_params
+    if @review.save
+      flash[:notice] = "Review added!"
+      redirect_to @book
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    book = Book.find params[:id]
+    book.destroy
+    respond_to do |format|
+      format.html { redirect_to :back }
+      format.json { render json: { status: :ok } }
+    end
+  end
+
+  private
+
+  def set_campus
+    @campus = Campus.find params[:campu_id]
+  end
+
+  def approved_params
+    params.require(:review).permit(:content, :user_id)
+  end
+
+
+end
