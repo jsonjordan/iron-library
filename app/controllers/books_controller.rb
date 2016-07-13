10ac08b5bbc18b1ff5a1d29041b93b90207177ab
Jason Jordan <jtjordan13@gmail.com>
2016-07-13 13:59:17 -0400

    fixed a few n+1 queries

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,8 @@
 class BooksController < ApplicationController
 
   def show
-    @book = Book.find params[:id]
+    @book = Book.find(params[:id])
+    @reviews = @book.reviews.includes(:user)
   end
 
   def index
