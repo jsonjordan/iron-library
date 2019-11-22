2ccc32b0d1ffca82b1c77939772f509851bad9c0
jsonjordan <jtjordan13@gmail.com>
2019-11-22 13:43:38 -0500

    updated gems and fixed non logged in user issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
     protected
 
         def configure_permitted_parameters
-          devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :current_password, :campus_id, :klass, :slack_name) }
-          devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :campus_id, :klass, :slack_name) }
+          devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :current_password, :campus_id, :klass, :slack_name])
+          devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :password, :current_password, :campus_id, :klass, :slack_name])
         end
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,7 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @reviews = @book.reviews.includes(:user)
+    @current_user = current_user
   end
 
   def index
