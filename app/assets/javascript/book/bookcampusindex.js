5447ac2d5a0e75349041e01d8333e050c682fa72
Jason Jordan <jtjordan13@gmail.com>
2022-05-14 19:34:09 -0400

    Screw jquery, Ill do it in erb!

--- app/assets/javascript/book/bookcampusindex.js
+++ app/assets/javascript/book/bookcampusindex.js
@@ -1,100 +1,93 @@
-$(document).on("page:change", function() {
-  var bookCount = $("#book-count")
-  var currentBooks = $("#current-books")
-
-  setTimeout(function(){
-    if($('body').is('.BookCampusIndex')){
-      getBooksCampus()
-  
-  
-    }
-  }, 2000);
-
-  // if($('body').is('.BookCampusIndex')){
-  //   getBooksCampus()
+// $(document).on("page:change", function() {
+//   var bookCount = $("#book-count")
+//   var currentBooks = $("#current-books")
 
 
-  // }
-
-})
-
-
-var getBooksCampus = function() {
-  var bookCount = $("#book-count")
-  var currentBooks = $("#current-books")
-  console.log('This part is')
-
-  $.ajax("/books.json", {
-    success: function(data) {
-
-      var length = data.bookInfo.length
-
-      bookCount.text(length)
-
-      for (var i=0; i < length; i ++) {
-        var book = data.bookInfo[i]
-
-        var newItem = $("<tr>")
-
-          var bookTitle = $("<td>")
-          bookTitle.text(book.title)
-          newItem.append(bookTitle)
-
-          var bookAuthor = $("<td>")
-          bookAuthor.text(book.author)
-          newItem.append(bookAuthor)
-
-          var bookCategory = $("<td>")
-          bookCategory.text(book.category)
-          newItem.append(bookCategory)
-
-          var bookStatus = $("<td>")
-          bookStatus.text(book.status)
-          newItem.append(bookStatus)
-
-          var bookButtons = $("<td>")
-          var gotoBookForm = $("<form>").attr('action', "/books/" + book.id)
-          var gotoBookButton = $("<button>").addClass("btn btn-info btn-xs").text("Go To Book")
-          gotoBookForm.append(gotoBookButton)
-          bookButtons.append(gotoBookForm)
-          newItem.append(bookButtons)
-
-          // var bookButtons = $("<td>")
-          // var editButton = $("<button>").addClass("edit-book btn btn-warning btn-xs").text("Edit").attr('data-book-id', book.id)
-          // var deleteButton = $("<button>").addClass("delete-book btn btn-danger btn-xs").text("Delete").attr('data-book-id', book.id)
-          // var checkOutButton = $("<button>").addClass("check-out-book btn btn-info btn-xs").text("Check Out").attr('data-book-id', book.id)
-          // var reserveButton = $("<button>").addClass("reserve-book btn btn-success btn-xs").text("Reserve").attr('data-book-id', book.id)
-          // bookButtons.append(deleteButton)
-          // bookButtons.append(editButton)
-          // bookButtons.append(checkOutButton)
-          // bookButtons.append(reserveButton)
-          // newItem.append(bookButtons)
+//   if($('body').is('.BookCampusIndex')){
+//     getBooksCampus()
 
 
-        currentBooks.append(newItem)
-      }
+//   }
 
-      listenForBookDeletes()
+// })
 
 
-    },
-    error: function() {
-      alert("No no no!")
-    }
-  })
-}
+// var getBooksCampus = function() {
+//   var bookCount = $("#book-count")
+//   var currentBooks = $("#current-books")
+//   console.log('This part is')
 
-function listenForBookDeletes() {
-  $(".delete-book").click(function() {
-    console.log("delete clicked")
-    var bookId = $(this).data("book-id")
-    $.ajax("/books/" + bookId + ".json", {
-      method: "DELETE",
-      success: function() {
-        $("#current-books").empty(),
-        getBooks()
-      },
-      error: function() { alert("Did not delete") }
-    })
-  })
-}
+//   $.ajax("/books.json", {
+//     success: function(data) {
+
+//       var length = data.bookInfo.length
+
+//       bookCount.text(length)
+
+//       for (var i=0; i < length; i ++) {
+//         var book = data.bookInfo[i]
+
+//         var newItem = $("<tr>")
+
+//           var bookTitle = $("<td>")
+//           bookTitle.text(book.title)
+//           newItem.append(bookTitle)
+
+//           var bookAuthor = $("<td>")
+//           bookAuthor.text(book.author)
+//           newItem.append(bookAuthor)
+
+//           var bookCategory = $("<td>")
+//           bookCategory.text(book.category)
+//           newItem.append(bookCategory)
+
+//           var bookStatus = $("<td>")
+//           bookStatus.text(book.status)
+//           newItem.append(bookStatus)
+
+//           var bookButtons = $("<td>")
+//           var gotoBookForm = $("<form>").attr('action', "/books/" + book.id)
+//           var gotoBookButton = $("<button>").addClass("btn btn-info btn-xs").text("Go To Book")
+//           gotoBookForm.append(gotoBookButton)
+//           bookButtons.append(gotoBookForm)
+//           newItem.append(bookButtons)
+
+//           // var bookButtons = $("<td>")
+//           // var editButton = $("<button>").addClass("edit-book btn btn-warning btn-xs").text("Edit").attr('data-book-id', book.id)
+//           // var deleteButton = $("<button>").addClass("delete-book btn btn-danger btn-xs").text("Delete").attr('data-book-id', book.id)
+//           // var checkOutButton = $("<button>").addClass("check-out-book btn btn-info btn-xs").text("Check Out").attr('data-book-id', book.id)
+//           // var reserveButton = $("<button>").addClass("reserve-book btn btn-success btn-xs").text("Reserve").attr('data-book-id', book.id)
+//           // bookButtons.append(deleteButton)
+//           // bookButtons.append(editButton)
+//           // bookButtons.append(checkOutButton)
+//           // bookButtons.append(reserveButton)
+//           // newItem.append(bookButtons)
+
+
+//         currentBooks.append(newItem)
+//       }
+
+//       listenForBookDeletes()
+
+
+//     },
+//     error: function() {
+//       alert("No no no!")
+//     }
+//   })
+// }
+
+// function listenForBookDeletes() {
+//   $(".delete-book").click(function() {
+//     console.log("delete clicked")
+//     var bookId = $(this).data("book-id")
+//     $.ajax("/books/" + bookId + ".json", {
+//       method: "DELETE",
+//       success: function() {
+//         $("#current-books").empty(),
+//         getBooks()
+//       },
+//       error: function() { alert("Did not delete") }
+//     })
+//   })
+// }
--- app/assets/javascript/book/bookindex.js
+++ app/assets/javascript/book/bookindex.js
@@ -1,95 +1,88 @@
-$(document).on('turbolinks:load', function() {
-  var bookCount = $("#book-count")
-  var currentBooks = $("#current-books")
-
-  setTimeout(function(){
-    if($('body').is('.BookCampusIndex')){
-      getBooks()
-  
-  
-    }
-  }, 2000);
-
-  // if($('body').is('.BookIndex')){
-  //   getBooks()
+// $(document).on('turbolinks:load', function() {
+//   var bookCount = $("#book-count")
+//   var currentBooks = $("#current-books")
 
 
-  // }
-
-})
-
-
-var getBooks = function() {
-  var bookCount = $("#book-count")
-  var currentBooks = $("#current-books")
-
-  $.ajax("/books.json", {
-    success: function(data) {
-
-      var length = data.bookInfo.length
-
-      bookCount.text(length)
-
-      for (var i=0; i < length; i ++) {
-        var book = data.bookInfo[i]
-
-        var newItem = $("<tr>")
-
-          var bookTitle = $("<td>")
-          bookTitle.text(book.title)
-          newItem.append(bookTitle)
-
-          var bookAuthor = $("<td>")
-          bookAuthor.text(book.author)
-          newItem.append(bookAuthor)
-
-          var bookCategory = $("<td>")
-          bookCategory.text(book.category)
-          newItem.append(bookCategory)
-
-          var bookCampus = $("<td>")
-          bookCampus.text(book.campus)
-          newItem.append(bookCampus)
-
-          var bookStatus = $("<td>")
-          bookStatus.text(book.status)
-          newItem.append(bookStatus)
-
-          var bookButtons = $("<td>")
-          var gotoBookForm = $("<form>").attr('action', "/books/" + book.id)
-          var gotoBookButton = $("<button>").addClass("btn btn-info btn-xs").text("Go To Book")
-          gotoBookForm.append(gotoBookButton)
-          bookButtons.append(gotoBookForm)
-          newItem.append(bookButtons)
-
-          // var bookButtons = $("<td>")
-          // var editButton = $("<button>").addClass("edit-book btn btn-warning btn-xs").text("Edit").attr('data-book-id', book.id)
-          // var deleteButton = $("<button>").addClass("delete-book btn btn-danger btn-xs").text("Delete").attr('data-book-id', book.id)
-          // var checkOutButton = $("<button>").addClass("check-out-book btn btn-info btn-xs").text("Check Out").attr('data-book-id', book.id)
-          // var reserveButton = $("<button>").addClass("reserve-book btn btn-success btn-xs").text("Reserve").attr('data-book-id', book.id)
-          // bookButtons.append(deleteButton)
-          // bookButtons.append(editButton)
-          // bookButtons.append(checkOutButton)
-          // bookButtons.append(reserveButton)
-          // newItem.append(bookButtons)
+//   if($('body').is('.BookIndex')){
+//     getBooks()
 
 
-        currentBooks.append(newItem)
-      }
+//   }
 
-      listenForBookGotos()
+// })
 
 
-    },
-    error: function() {
-      alert("No no no!")
-    }
-  })
-}
+// var getBooks = function() {
+//   var bookCount = $("#book-count")
+//   var currentBooks = $("#current-books")
 
-function listenForBookGotos() {
-  $(".goto-book").click(function() {
-    var bookId = $(this).data("book-id")
+//   $.ajax("/books.json", {
+//     success: function(data) {
 
-  })
-}
+//       var length = data.bookInfo.length
+
+//       bookCount.text(length)
+
+//       for (var i=0; i < length; i ++) {
+//         var book = data.bookInfo[i]
+
+//         var newItem = $("<tr>")
+
+//           var bookTitle = $("<td>")
+//           bookTitle.text(book.title)
+//           newItem.append(bookTitle)
+
+//           var bookAuthor = $("<td>")
+//           bookAuthor.text(book.author)
+//           newItem.append(bookAuthor)
+
+//           var bookCategory = $("<td>")
+//           bookCategory.text(book.category)
+//           newItem.append(bookCategory)
+
+//           var bookCampus = $("<td>")
+//           bookCampus.text(book.campus)
+//           newItem.append(bookCampus)
+
+//           var bookStatus = $("<td>")
+//           bookStatus.text(book.status)
+//           newItem.append(bookStatus)
+
+//           var bookButtons = $("<td>")
+//           var gotoBookForm = $("<form>").attr('action', "/books/" + book.id)
+//           var gotoBookButton = $("<button>").addClass("btn btn-info btn-xs").text("Go To Book")
+//           gotoBookForm.append(gotoBookButton)
+//           bookButtons.append(gotoBookForm)
+//           newItem.append(bookButtons)
+
+//           // var bookButtons = $("<td>")
+//           // var editButton = $("<button>").addClass("edit-book btn btn-warning btn-xs").text("Edit").attr('data-book-id', book.id)
+//           // var deleteButton = $("<button>").addClass("delete-book btn btn-danger btn-xs").text("Delete").attr('data-book-id', book.id)
+//           // var checkOutButton = $("<button>").addClass("check-out-book btn btn-info btn-xs").text("Check Out").attr('data-book-id', book.id)
+//           // var reserveButton = $("<button>").addClass("reserve-book btn btn-success btn-xs").text("Reserve").attr('data-book-id', book.id)
+//           // bookButtons.append(deleteButton)
+//           // bookButtons.append(editButton)
+//           // bookButtons.append(checkOutButton)
+//           // bookButtons.append(reserveButton)
+//           // newItem.append(bookButtons)
+
+
+//         currentBooks.append(newItem)
+//       }
+
+//       listenForBookGotos()
+
+
+//     },
+//     error: function() {
+//       alert("No no no!")
+//     }
+//   })
+// }
+
+// function listenForBookGotos() {
+//   $(".goto-book").click(function() {
+//     var bookId = $(this).data("book-id")
+
+//   })
+// }
