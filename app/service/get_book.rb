6ca8977958728ef725446f28caf2c68efd950c11
Jason Jordan <jtjordan13@gmail.com>
2022-05-16 13:14:53 -0400

    refactored method that call library api

--- app/service/get_book.rb
+++ app/service/get_book.rb
@@ -22,71 +22,82 @@ class GetBook
     # else
     #   isbndb = r["data"].first["summary"]
     # end
-    response = HTTParty.get("https://www.googleapis.com/books/v1/volumes?q=isbn:#{@isbn}")
+    response = googleapis_call
 
+    raw_book_info = response['items'][0]['volumeInfo']
 
-
-    book_info = response['items'][0]['volumeInfo']
-    # isbndb = 'Book summary not avaliabe at this time'
-
-    if book_info['imageLinks']
-      pic_url = book_info['imageLinks']['thumbnail']
-    else
-      pic_url = "/no-cover.gif"
-    end
-
-    if book_info['description']
-      book_summary = book_info['description']
-    else
-      book_summary = 'Book summary not available at this time'
-    end
+    book_info = check_for_missing_fields(raw_book_info)
 
     book = Book.create(isbn: @isbn,
-                     title: book_info['title'],
-                     author: book_info['authors'][0],
-                     summary: book_info['description'],
-                     year_of_publication: book_info['publishedDate'],
-                     cover_url: pic_url,
-                     data: response )
+                      title: book_info['title'],
+                      author: book_info['author'],
+                      summary: book_info['summary'],
+                      year_of_publication: book_info['year_of_publication'],
+                      cover_url: book_info['cover_url'],
+                      data: response
+                      )
     book
   end
 
   def update
-    response = HTTParty.get("https://www.googleapis.com/books/v1/volumes?q=isbn:#{@isbn}")
+    response = googleapis_call
 
-    book_info = response['items'][0]['volumeInfo']
+    raw_book_info = response['items'][0]['volumeInfo']
 
-    if book_info['imageLinks']
-      pic_url = book_info['imageLinks']['thumbnail']
-    else
-      pic_url = "/no-cover.gif"
-    end
-
-    if book_info['description']
-      book_summary = book_info['description']
-    else
-      book_summary = 'Book summary not available at this time'
-    end
+    book_info = check_for_missing_fields(raw_book_info)
 
     book = Book.find_by(isbn: @isbn)
     book = book.update(
                      title: book_info['title'],
-                     author: book_info['authors'][0],
-                     summary: book_summary,
-                     year_of_publication: book_info['publishedDate'],
-                     cover_url: pic_url,
+                     author: book_info['author'],
+                     summary: book_info['summary'],
+                     year_of_publication: book_info['year_of_publication'],
+                     cover_url: book_info['cover_url'],
                      data: response
                      )
   end
 
   def title_author
     info = []
-    response = HTTParty.get("https://www.googleapis.com/books/v1/volumes?q=isbn:#{@isbn}")
+    response = googleapis_call
 
-    book_info = response['items'][0]['volumeInfo']
+    raw_book_info = response['items'][0]['volumeInfo']
+
+    book_info = check_for_missing_fields(raw_book_info)
 
     info.push(book_info["title"])
-    info.push(book_info["authors"][0])
+    info.push(book_info["author"])
     info
+  end
+
+  private
+
+  def check_for_missing_fields(book_info)
+    fields = {
+      'title' => book_info['title'],
+      'year_of_publication' => book_info['publishedDate']
+    }
+    if book_info['authors']
+      fields['author'] = book_info['authors'][0]
+    else
+      fields['author'] = 'Unknown'
+    end
+    
+    if book_info['imageLinks']
+      fields['cover_url'] = book_info['imageLinks']['thumbnail']
+    else
+      fields['cover_url'] = "/no-cover.gif"
+    end
+
+    if book_info['description']
+      fields['summary'] = book_info['description']
+    else
+      fields['summary'] = 'Book summary not available at this time'
+    end
+    fields
+  end
+
+  def googleapis_call
+    HTTParty.get("https://www.googleapis.com/books/v1/volumes?q=isbn:#{@isbn}")
   end
 end
